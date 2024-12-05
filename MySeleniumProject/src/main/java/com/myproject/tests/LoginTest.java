2ce38f4fb558d0f58f71675087bdc63064ccf954
Raj3015 <boss4aws@gmail.com>
2024-12-05 17:43:31 +0530

    Update LoginTest.java

--- MySeleniumProject/src/main/java/com/myproject/tests/LoginTest.java
+++ MySeleniumProject/src/main/java/com/myproject/tests/LoginTest.java
@@ -1,56 +1,31 @@
-package com.myproject.tests;
+package SeleniumProject.Maven;
 
+
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-import org.testng.annotations.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class LoginTest {
-
-    WebDriver driver;
-
-    @BeforeClass
-    public void setup() {
-        // You can use WebDriverManager to handle the driver automatically (recommended)
-        // WebDriverManager.chromedriver().setup();
-        
-        // If you prefer to manually set up the driver path:
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
-    }
-
-    @Test
-    public void testLoginPageTitle() {
-        driver.get("https://www.example.com/login");
-        
-        String pageTitle = driver.getTitle();
-        Assert.assertEquals(pageTitle, "Login Page");
-    }
-
-    @Test
-    public void testLoginFunctionality() {
-        driver.get("https://www.example.com/login");
-
-        // Assuming the login form has fields with ids "username" and "password", and a submit button with the id "loginBtn"
-        WebElement usernameField = driver.findElement(By.id("username"));
-        WebElement passwordField = driver.findElement(By.id("password"));
-        WebElement loginButton = driver.findElement(By.id("loginBtn"));
-
-        usernameField.sendKeys("testuser");
-        passwordField.sendKeys("testpassword");
-        loginButton.click();
-
-        // Assuming successful login redirects to the dashboard page
-        String currentUrl = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrl, "https://www.example.com/dashboard");
-    }
-
-    @AfterClass
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+public class AppTest 
+{ 
+	public WebDriver driver; 
+	public String baseUrl = "https://www.lambdatest.com/";  
+	
+	@Test             
+	public void test1() {      
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
+ 
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);  
+		driver.manage().window().maximize();  
+		driver.get(baseUrl);
+		driver.close();
+	}     
 }
