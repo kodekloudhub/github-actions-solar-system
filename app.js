b72dfd39785a5f2892c61b644d5a5c3bcc7b9833
Rob Thorne <rob@torenware.com>
2025-02-14 18:36:05 -0800

    test removing funky reporter

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const app = express();
 const cors = require('cors')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 
 app.use(bodyParser.json());
