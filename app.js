f56b14ce0feb5b53590e655c863181ae7d5fb6b2
rukevweubio22 <ubiorukevwe@gmail.com>
2025-04-11 11:57:48 +0000

    add

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
+    user: superuser,
+    pass: Superpassword,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
